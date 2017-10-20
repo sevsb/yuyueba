0c5368db2d05777d596e01fe747be1221131ffe1
logic <923132714@qq.com>
2017-10-20 18:18:20 +0800

    add TCmessage test

---
+++ controller/api/v1/sms_controller.php
@@ -0,0 +1,48 @@
+<?php
+include_once(dirname(__FILE__) . "/../../../config.php");
+include_once(dirname(__FILE__) . "/../v1_base.php");
+include_once(dirname(__FILE__) . "/app/TCmessage.class.php");
+class sms_controller extends v1_base {
+    public function preaction($action) {
+    }
+   public function send_action() {
+	   $nationCode = get_request('nationCode');
+        $phoneNumber = get_request('phoneNumber');
+        $yuyue_session = get_request('yuyue_session');
+        $token = get_request('token');
+		$verification_code = rand(1000,9999)
+		$msg = "【预约一下】您的验证码是：".$verification_code;
+$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
+$data = $sender->send(0, $nationCode, $phoneNumber, $msg, $extend = "", $ext = "");
+ logging::d("sendsms", "sms msg is:" . $msg);
+  return array("data" =>  $data);
+   }
+   
+  public function verification_action() {
+	   $nationCode = get_request('nationCode');
+        $phoneNumber = get_request('phoneNumber');
+        $yuyue_session = get_request('yuyue_session');
+        $token = get_request('token');
+		$verification_code = rand(1000,9999)
+		$msg = "【预约一下】您的验证码是：".$verification_code;
+$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
+$data = $sender->send(0, $nationCode, $phoneNumber, $msg, $extend = "", $ext = "");
+ logging::d("sendsms", "sms msg is:" . $msg);
+  return array("data" =>  $data);
+   }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

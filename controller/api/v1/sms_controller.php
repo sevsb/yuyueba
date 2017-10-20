6bec8a4e5132c0212a652d2b12abba94c3d4df55
logic <923132714@qq.com>
2017-10-20 19:26:12 +0800

    test

--- controller/api/v1/sms_controller.php
+++ controller/api/v1/sms_controller.php
@@ -16,6 +16,8 @@ class sms_controller extends v1_base {
 		$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
 		$data = $sender->send(0, $nationCode, $phoneNumber, $msg, $extend = "", $ext = "");
 		logging::d("sendsms", "sms msg is:" .urldecode($msg));
+		echo $msg;
+		echo '<br/>'
 	return array("data" =>  $data);
    }
    

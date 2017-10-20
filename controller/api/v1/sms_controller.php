237a2016295f7c5fe979af16991640265633771e
logic <923132714@qq.com>
2017-10-20 20:18:28 +0800

    test

--- controller/api/v1/sms_controller.php
+++ controller/api/v1/sms_controller.php
@@ -15,29 +15,15 @@ class sms_controller extends v1_base {
 		logging::d("sendsms", "phoneNumber is:" .$phoneNumber);
 		
 		$verification_code = rand(1000,9999)
-		$msg = utf8_encode("123456");
+		$message ="123456";
 		
 		$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
-		$data = $sender->send(0, $nationCode, $phoneNumber, $msg, $extend = "", $ext = "");
+		$data = $sender->send(0, $nationCode, $phoneNumber, $message, $extend = "", $ext = "");
 	
-		echo $msg;
-		echo '<br/>'
 	return array("data" =>  $data);
    }
    
-  public function verification_action() {
-	   $nationCode = get_request('nationCode');
-        $phoneNumber = get_request('phoneNumber');
-        $yuyue_session = get_request('yuyue_session');
-        $token = get_request('token');
-		$verification_code = rand(1000,9999)
-		$msg = "【预约一下】您的验证码是：".$verification_code;
-		$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
-		$data = $sender->send(0, $nationCode, $phoneNumber, $msg, $extend = "", $ext = "");
-		logging::d("sendsms", "sms msg is:" . $msg);
-  return array("data" =>  $data);
-   }
-
+  
 
 }
 

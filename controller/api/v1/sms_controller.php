1cb1c287ee91cbf10232f045dadc90ee2984d8e5
logic <923132714@qq.com>
2017-10-20 20:38:17 +0800

    test

--- controller/api/v1/sms_controller.php
+++ controller/api/v1/sms_controller.php
@@ -13,12 +13,12 @@ class sms_controller extends v1_base {
 		logging::d("sendsms", "nationCode is:" .$nationCode);
 		logging::d("sendsms", "phoneNumber is:" .$phoneNumber);
 		
-		$verification_code = rand(1000,9999)
+		$verification_code = rand(1000,9999);
 	
 		
-		//$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
-		//$data = $sender->send(0, $nationCode, $phoneNumber, "123456", "", "");
-	//$data=json_decode($data)
-	//return array("data" =>  $data);
+		$sender = new SmsSingleSender( WX_APPID,WX_SECRET);
+		$data = $sender->send(0, $nationCode, $phoneNumber, "123456", "", "");
+	$data=json_decode($data)
+	return array("data" =>  $data);
    }
 }

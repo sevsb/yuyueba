a48ad976e2ec6e9fd865b74f67fe6b58e4c57950
Logic <923132714@qq.com>
2017-10-31 14:31:25 +0800

    sms test

--- controller/api/v1/sms_controller.php
+++ controller/api/v1/sms_controller.php
@@ -16,7 +16,7 @@ class sms_controller extends v1_base {
 		logging::d("sendsms", "phoneNumber is:" .$phoneNumber);
 		
 		$verification_code = rand(1000,9999);
-	$params =[{$verification_code }];
+	$params =[""+$verification_code];
 		
 		$sender = new SmsSingleSender( WX_SMS_SDKID,WX_SMS_SECRET);
 		$data = $sender->sendWithParam($nationCode, $phoneNumber, $templId, $params);

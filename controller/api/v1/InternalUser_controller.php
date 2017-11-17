bc45c34cd72e5fe4fcd5435dfc75e9e36dfe2762
Logic <923132714@qq.com>
2017-11-17 09:34:23 +0800

    手机端有问题再次测试

--- controller/api/v1/InternalUser_controller.php
+++ controller/api/v1/InternalUser_controller.php
@@ -17,11 +17,11 @@ public function send_action(){
 if (empty($user)) {//如果没有对应的user，就创建一个。
 			$user = new InternalUser();
 		}
-/*
+
 logging::d("Id", "Id is:" .$user->id());
 logging::d("Id", "Id is:" .$user->verify_code());
 logging::d("Id", "Id is:" .$user->verify_status());
-*/
+
 
 		
 		$tempId = $tempuser->id();//获取对应tempid
@@ -29,11 +29,11 @@ logging::d("Id", "Id is:" .$user->verify_status());
 		if($user->id()==0){//未注册
 				
 			$user->setTempId($tempId);
-		//	logging::d("tempId", "tempId is:" .$tempId);
+			logging::d("tempId", "tempId is:" .$tempId);
 			$user->setTelephone($phoneNumber);
-			/*logging::d("sendsms", "nationCode is:" .$nationCode);
+			logging::d("sendsms", "nationCode is:" .$nationCode);
 			logging::d("sendsms", "phoneNumber is:" .$phoneNumber);
-		*/
+	
 		}
 		else if($user->id()!=0){//已注册
 			if($user->verify_status()=="true"){//已注册成功，登陆
@@ -50,7 +50,7 @@ logging::d("Id", "Id is:" .$user->verify_status());
 				
 				}else{//绑定有问题
 					$user->setTempId($tempId);
-		//	logging::d("tempId", "tempId is:" .$tempId);
+			logging::d("tempId", "tempId is:" .$tempId);
 				}
 		
 			}
@@ -59,7 +59,7 @@ logging::d("Id", "Id is:" .$user->verify_status());
 			
 			$verification_code = rand(1000,9999);//随机验证码
 			$user->setCode($verification_code);
-		//	logging::d("sendsms", "verification_code is:" .$verification_code);
+			logging::d("sendsms", "verification_code is:" .$verification_code);
 			
 			$params =array("".$verification_code);
 
@@ -77,57 +77,19 @@ logging::d("Id", "Id is:" .$user->verify_status());
    }
 
    public function verify_action() {
-	   /**
-	    $from = get_request('from');
-        logging::d("LOGIN", "FROM:" . $from);
-        if ($from == 'weapp') { //具体的来源，现在只有微信小程序，也就是WXAPP
-
-		
-            $yuyue_session = get_request('yuyue_session', "");  //yuyue_session用作传递的userid
-            $avatar = get_request('avatar', "");  //
-            $nick = get_request('nick', "");  //
-            logging::d("LOGIN", "nick:" . $nick);
-            logging::d("LOGIN", "avatar:" . $avatar);
-            $user = InternalUser::oneBySession($yuyue_session); //拿到具体的tempuser信息,tempuser是wx小程序的user,
-            if (empty($user)) {                             //如果没有对应的user，就创建一个。    
-                $code = get_request('code', '');
-                $wx_auth_ret = Wxapi::wx_auth($code);   //获取openid
-                if (!empty($wx_auth_ret->errcode)){
-                    return array('op' => 'fail', 'code' => $wx_auth_ret->errcode, 'reason' => $wx_auth_ret->errmsg);
-                }
-                $openid = $wx_auth_ret->openid;
-                $session_key = $wx_auth_ret->session_key;
-                $yuyue_session = md5(time() . $openid . $session_key);
-                $token = md5(time());
-
-                $user = InternalUser::createByOpenid($openid);  //创建TempUser,修改属性，保存
-                $user->setOpenId($openid);
-                $user->setSessionKey($session_key);
-                $user->setToken($token);
-                $user->setYuyueSession($yuyue_session);
-                logging::d("LOGIN", "yuyue_session now is :" . $yuyue_session);
-            }
-            $user->setAvatar($avatar);
-            $user->setNickname($nick);
-            $user->save();
-            //logging::d("LOGIN", "now user is :" . $user);
-            $data = new stdClass();
-            $data->timeout = time() + 7200;
-            $data->token = $user->token();
-            $data->yuyue_session = $user->yuyue_session();
-            //logging::d("LOGIN", "now data is :" . $data);
-            return array("op" => "login", 'data' => $data);
-        }*/
-	   
-	   
-	   
+ 
 	   $nationCode = get_request('nationCode');
         $phoneNumber = get_request('phoneNumber');
         $yuyue_session = get_request('yuyue_session');
-        $token = get_request('token');
-		$templId = 50285;
+		  $verify_code = get_request('verify_code');
+		
 
-		$user = new InternalUser();
+
+		$user = InternalUser::oneByTelephone($phoneNumber);//通过手机号 获取对应的内部用户
+		if (empty($user)) {//如果没有对应的user，就创建一个。
+			$user = new InternalUser();
+		}
+	
 		$user->setTelephone($phoneNumber);
 		$user->setYuyueSession(yuyue_session);
 		logging::d("sendsms", "nationCode is:" .$nationCode);
